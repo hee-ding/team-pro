b797dd5c3ca4edea49b3cfc2f905b3c141683c8f
tianahyeon <xogus1484@naver.com>
2023-02-10 00:43:22 +0900

    [UPDATE] MaumgagymApplication - basePackages 추가

--- src/main/java/com/maumgagym/MaumgagymApplication.java
+++ src/main/java/com/maumgagym/MaumgagymApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan( basePackages = { "com.maumgagym.controller", "com.maumgagym.dao", "com.maumgagym.dto","com.maumgagym.service" })
+@ComponentScan( basePackages = { "com.maumgagym", "com.maumgagym.controller", "com.maumgagym.dao", "com.maumgagym.dto","com.maumgagym.service" })
 public class MaumgagymApplication {
 
 	public static void main(String[] args) {
