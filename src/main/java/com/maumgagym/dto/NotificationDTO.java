eed43d1a2a84c844b295a74f15106dce75e895aa
tianahyeon <xogus1484@naver.com>
2023-02-26 17:06:09 +0900

    [INSERT] 공지/이벤트 DTO 생성

---
+++ src/main/java/com/maumgagym/dto/NotificationDTO.java
@@ -0,0 +1,21 @@
+package com.maumgagym.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class NotificationDTO {
+	
+	int seq;
+	int category_seq;
+	String subject;
+	int writer_admin;
+	String date;
+	int hit;
+	String content;
+
+	String writer;
+	String category;
+	
+}
